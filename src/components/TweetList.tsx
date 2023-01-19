9b4d7d2b78f417b15499eac4163b1bb1c2976604
rmbh4211995 <rmbh4211995@gmail.com>
2023-01-19 00:07:42 -0500

    Add button that links to original tweet

--- src/components/TweetList.tsx
+++ src/components/TweetList.tsx
@@ -1,6 +1,6 @@
 import { format } from 'date-fns'
 import { Icon } from 'solid-heroicons'
-import { trash } from 'solid-heroicons/outline'
+import { link, trash } from 'solid-heroicons/outline'
 import type { JSX } from 'solid-js'
 import { For } from 'solid-js'
 
@@ -16,7 +16,7 @@ const Tweet = (props: { tweet: Tweet; idx: number; onDelete: any }) => (
     <td class='w-10'>{props.idx}</td>
     <td class=''>{props.tweet.text}</td>
     <td class=''>{format(new Date(props.tweet.created_at), 'MMM dd, yyyy, HH:mm aa')}</td>
-    <td>
+    <td class='flex space-x-3'>
       <button
         id={props.tweet.id}
         title='Delete'
@@ -24,6 +24,14 @@ const Tweet = (props: { tweet: Tweet; idx: number; onDelete: any }) => (
         class='text-blue-500 hover:text-blue-800'
       >
         <Icon path={trash} class='h-6 w-6 text-inherit' />
+      </button>
+      <button
+        title='Go to tweet'
+        // https://developer.twitter.com/en/blog/community/2020/getting-to-the-canonical-url-for-a-tweet
+        onClick={() => window.open(`https://twitter.com/twitter/status/${props.tweet.id}`)}
+        class='text-blue-500 hover:text-blue-800'
+      >
+        <Icon path={link} class='h-6 w-6 text-inherit' />
       </button>
     </td>
   </tr>
