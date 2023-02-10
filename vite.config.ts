c70d4951961386a5d9420dcfaadb54b6fce44ba3
rmbh4211995 <rmbh4211995@gmail.com>
2023-02-09 20:53:00 -0500

    Enable vercel edge

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import solid from 'solid-start/vite'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  plugins: [solid({ adapter: vercel({ edge: false }) })],
+  plugins: [solid({ adapter: vercel({ edge: true }) })],
   ssr: { external: ['@prisma/client'] },
 })
