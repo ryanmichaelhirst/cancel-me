4b7e08f6f01645ff1b5188e2751a5e84ae2e50f2
rmbh4211995 <rmbh4211995@gmail.com>
2023-02-11 16:09:58 -0500

    Try using donations()

--- src/routes/[...index]/user/[id].tsx
+++ src/routes/[...index]/user/[id].tsx
@@ -11,12 +11,12 @@ import { ProfanityScoreCard } from '~/components/profanity-score-card'
 import { ProgressBar } from '~/components/progress-bar'
 import { Tweet } from '~/components/tweet'
 import type { ProfanityMetrics, Tweet as TweetRecord } from '~/types'
-import { credentials } from '~/util'
+import { credentials, donations } from '~/util'
 
 export const routeData = ({ params }: RouteDataArgs) => {
   return createServerData$(
     async ([, userId], { request }) => {
-      return { credentials: credentials(), donations: [] }
+      return { credentials: credentials(), donations: await donations({ userId }) }
     },
     { key: () => ['donations', params.id] },
   )
